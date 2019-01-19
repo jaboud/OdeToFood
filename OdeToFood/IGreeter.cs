22d042d92af55711d94d9c9bbbaf023a7909353c
Justin Aboud <jaboud94@gmail.com>
2019-01-19 17:13:06 +1100

    Initial Commit

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+
+    // Dependency Injection is the automatic creation of classes and their injection into other classes through a constructor.
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        public string GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+
+        private IConfiguration _configuration;
+        public Greeter(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+       
+
+    }
+
+    
+
+
+}
