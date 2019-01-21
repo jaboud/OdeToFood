0e414a6a03b26d751212b66a465679168859d89c
jaboud <jaboud94@gmail.com>
2019-01-22 00:13:56 +1100

    Updated

--- OdeToFood/Models/Restaurant.cs
+++ OdeToFood/Models/Restaurant.cs
@@ -5,9 +5,17 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Models
 {
+    public enum CuisineType
+    {
+        None,
+        Italian,
+        French,
+        American
+    }
     public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public CuisineType Cuisine { get; set; }
     }
 }
